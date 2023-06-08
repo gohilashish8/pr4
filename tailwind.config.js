a65a437561adf0a195c537622f68d5b9923e5454
Ashish Smarttehchnica <ashish.smarttechnica@gmail.com>
2023-06-08 07:23:03 +0530

    pr-4

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+]
+}
